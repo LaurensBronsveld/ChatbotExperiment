d936691b70faecf2e16ee1dae698572e2c44922d
497506@student.saxion.nl <u&`!zOI;d9?6vxP%;;$=>
2025-03-05 10:35:16 +0100

    removed pycache files

--- app/agents/LLMs.py
+++ app/agents/LLMs.py
@@ -3,8 +3,7 @@ from pydantic_ai import Agent, RunContext
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.models.anthropic import AnthropicModel
-from models.models import ResponseDict, HandbookChunk
-from components.system_prompts import get_chatbot_prompt
+
 
 def get_model(provider: str, name: str):
     if provider.lower() == "open-ai":
